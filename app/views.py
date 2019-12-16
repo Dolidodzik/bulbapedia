00b91d7f86ee1b8a0092b8ad68cf32e6836fd52f
Dominik Lech <teethtard321@gmail.com>
2019-12-16 19:00:52 +0000

    Setup basic searching

--- app/views.py
+++ app/views.py
@@ -7,4 +7,5 @@ def homepage(request):
     return render(request, "home.html", {"header": hmi.name, "text": hmi.text})
 
 def searchpage(request, search_query):
-    return render(request, "search_results.html", {"search_query": search_query})
+    # Currently only name searching support
+    return render(request, "search_results.html", {"search_query": search_query, "results": Creature.objects.filter(Breed_name__contains=search_query)})
