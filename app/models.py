9a099338f47246794170905c4c03670edab3320d
Dominik Lech <teethtard321@gmail.com>
2019-12-16 15:42:19 +0000

    Created HomePage model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class HomePage(BaseModel):
+    text = models.TextField(default="This text will be displayed on homepage of your site")
+
+    def save(self, *args, **kwargs):
+        # Checking if user didn't try to add more than one HomePage, just for safety
+        if not self.pk and HomePage.objects.exists():
+            raise ValidationError('There is can be only one HomePage instance, do not add more than one instance of this, you can edit an existing one as you want!')
+        return super(HomePage, self).save(*args, **kwargs)
