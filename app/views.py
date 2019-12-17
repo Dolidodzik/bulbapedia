a65a96e9c65a27e29f8079ca76a4de15f3aec260
Dominik Lech <teethtard321@gmail.com>
2019-12-17 18:40:56 +0000

    Searching now is able to see every Creature model field

--- app/views.py
+++ app/views.py
@@ -7,9 +7,30 @@ def homepage(request):
     return render(request, "home.html", {"header": hmi.name, "text": hmi.text})
 
 def searchpage(request, search_query):
-    # Currently only name searching support, just for testing purpooses
-    return render(request, "search_results.html", {"search_query": search_query, "results": Creature.objects.filter(Breed_name__contains=search_query)})
+    data = [
+        Creature.objects.filter(Breed_name__icontains=search_query),
+        Creature.objects.filter(Type__icontains=search_query),
+        Creature.objects.filter(Element__icontains=search_query),
+        Creature.objects.filter(Frequency__icontains=search_query),
+        Creature.objects.filter(Diet__icontains=search_query),
+        Creature.objects.filter(Role__icontains=search_query),
+        Creature.objects.filter(Endurance__icontains=search_query),
+        Creature.objects.filter(Hunger__icontains=search_query),
+        Creature.objects.filter(Strong_VS__icontains=search_query),
+        Creature.objects.filter(Weak_VS__icontains=search_query),
+        Creature.objects.filter(Attacks__icontains=search_query),
+        Creature.objects.filter(Strength__icontains=search_query),
+        Creature.objects.filter(Speed__icontains=search_query),
+        Creature.objects.filter(Agility__icontains=search_query),
+        Creature.objects.filter(Durability__icontains=search_query),
+        Creature.objects.filter(Other_Enchancements__icontains=search_query),
+        Creature.objects.filter(Evolves_from__icontains=search_query),
+        Creature.objects.filter(Evolves_to__icontains=search_query),
+        Creature.objects.filter(Fluff__icontains=search_query),
+    ]
+
+    return render(request, "search_results.html", {"search_query": search_query, "results": data })
 
 def subpage(request, creature_name):
-    creature_data = Creature.objects.filter(Breed_name__iexact=creature_name).first()
+    creature_data = Creature.objects.filter(Role__iexact=creature_name).first()
     return render(request, "subpage.html", {"creature_name": creature_name, "creature_data": creature_data} )
