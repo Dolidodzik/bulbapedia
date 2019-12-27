076bcff397d799e01e4ea3fb0f24698c8311bde0
Dominik Lech <teethtard321@gmail.com>
2019-12-27 12:33:30 +0000

    Allowed spaces in /creature/<creature_name> using re_path

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from app.views import *
 
 urlpatterns = [
     path('', HomePageView.as_view()),
     path('search/<slug:search_query>/', searchpage),
-    path('creature/<slug:creature_name>/', subpage),
+    #path('creature/<slug:creature_name>/', subpage),
+    re_path(r'^creature/(?P<creature_name>[\w|\W]+)', subpage),
     path('advanced_search/', AdvancedSearchView.as_view()),
 ]
